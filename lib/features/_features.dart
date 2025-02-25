e44e3fe9a8d02a67ce98a68282edebfa423a51af
Naser Sami <naser_ebedo@icloud.com>
2025-02-25 13:22:15 +0300

    ShopZen

--- lib/features/_features.dart
+++ lib/features/_features.dart
@@ -1,10 +1,10 @@
-export 'copy_me/_copy_me.dart';
+export 'cart/_cart.dart';
+export 'chat/_chat.dart';
+export 'home/_home.dart';
+export 'saved/_saved.dart';
+export 'account/_account.dart';
+export '_copy_me/_copy_me.dart';
 export 'splash/splash_screen.dart';
 export 'onboarding/onboarding_screen.dart';
 export 'authentication/_authentication.dart';
 export 'bottom_navigation_bar/bottom_navigation_bar.dart';
-export 'home/_home.dart';
-export 'saved/_saved.dart';
-export 'account/_account.dart';
-export 'cart/_cart.dart';
-export 'chat/_chat.dart';
