a69872081d77ab158f3c73d0bb2b38c4ddd3e9b4
Naser Sami <naser_ebedo@icloud.com>
2025-03-05 00:42:36 +0300

    ShopZen

--- lib/features/address/presentation/screen/add_new_address_screen.dart
+++ lib/features/address/presentation/screen/add_new_address_screen.dart
@@ -1,14 +1,16 @@
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:geolocator/geolocator.dart';
 import 'package:go_router/go_router.dart';
 import 'package:place_picker_google/place_picker_google.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-import '/core/_core.dart' show IGeolocatorService, sl;
-import '/config/_config.dart' show NotificationsIconWidget, TPadding, TextWidget, TSize;
+import '/core/_core.dart' show IGeolocatorService, sl, TValidator;
+import '/config/_config.dart'
+    show TPadding, TextWidget, TSize, TRadius, TextFormFieldComponent;
 
 class AddNewAddressScreen extends StatefulWidget {
   static const String routeName = 'new-address';
@@ -20,8 +22,31 @@ class AddNewAddressScreen extends StatefulWidget {
 
 class _AddNewAddressScreenState extends State<AddNewAddressScreen> {
   LatLng latLng = const LatLng(0, 0);
-  // String? mapStyle;
-  String apiKey = '';
+  String apiKey = 'AIzaSyDHHc9azYoOJWCgw7t-EkbrhOlRuLOiW2M';
+
+  String? mapStyle;
+  bool isDefaultAddress = false;
+
+  final _addressNicknameController = TextEditingController();
+  final _fullAddressController = TextEditingController();
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _loadMapStyle();
+  }
+
+  Future<void> _loadMapStyle() async {
+    final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+    final stylePath = isDarkMode
+        ? 'assets/map/map_style_night.json'
+        : 'assets/map/map_style_light.json';
+
+    final style = await rootBundle.loadString(stylePath);
+    setState(() {
+      mapStyle = style;
+    });
+  }
 
   @override
   void initState() {
@@ -29,43 +54,28 @@ class _AddNewAddressScreenState extends State<AddNewAddressScreen> {
     super.initState();
   }
 
-  @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    // _loadMapStyle();
-  }
-
   Future<Position> _getCurrentLocation() async {
     await sl<IGeolocatorService>().requestPermission();
     return await sl<IGeolocatorService>().getCurrentPosition();
   }
 
-  // Future<void> _loadMapStyle() async {
-  //   final stylePath = TFunctions.isDarkMode(context)
-  //       ? 'assets/map/map_style_night.json'
-  //       : 'assets/map/map_style_light.json';
+  void _onIsDefaultAddressChanged(bool value) {
+    // setState(() {
+    isDefaultAddress = value;
+    // });
+  }
 
-  //   final style = await rootBundle.loadString(stylePath);
-  //   setState(() {
-  //     mapStyle = style;
-  //   });
-  // }
+  @override
+  void dispose() {
+    super.dispose();
+    _addressNicknameController.dispose();
+    _fullAddressController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('New Address'),
-        actions: const [NotificationsIconWidget()],
-        bottom: PreferredSize(
-          preferredSize: Size(MediaQuery.of(context).size.width, 10),
-          child: const Padding(
-            padding: EdgeInsets.symmetric(horizontal: TPadding.p20),
-            child: Divider(),
-          ),
-        ),
-      ),
       body: FutureBuilder<Position>(
         future: _getCurrentLocation(),
         builder: (context, snapshot) {
@@ -79,23 +89,136 @@ class _AddNewAddressScreenState extends State<AddNewAddressScreen> {
 
           return PlacePicker(
             apiKey: apiKey,
-            // mapsBaseUrl: mapStyle,
+            myLocationEnabled: true,
             myLocationButtonEnabled: true,
+            selectedPlaceWidgetBuilder: (context, state, selectedPlace) {
+              _fullAddressController.text = selectedPlace?.name.toString() ?? '';
+              return GestureDetector(
+                onTap: () {
+                  showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        height: 450,
+                        decoration: BoxDecoration(
+                          color: theme.colorScheme.surface,
+                          borderRadius: const BorderRadius.only(
+                            topLeft: Radius.circular(TRadius.r16),
+                            topRight: Radius.circular(TRadius.r16),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(TPadding.p20).copyWith(top: 0),
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 6,
+                                width: 64,
+                                margin:
+                                    const EdgeInsets.symmetric(vertical: TPadding.p16),
+                                decoration: BoxDecoration(
+                                  color:
+                                      theme.colorScheme.outline.withValues(alpha: 0.20),
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  TextWidget(
+                                    'Address',
+                                    style: theme.textTheme.titleLarge,
+                                  ),
+                                  IconButton(
+                                    onPressed: () => context.pop(),
+                                    icon: const Icon(
+                                      Icons.close,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              const SizedBox(height: TSize.s16),
+                              TextFormFieldComponent(
+                                textFieldWithTitle: true,
+                                title: 'Address Nickname',
+                                hintText: 'Choose a nickname.',
+                                controller: _addressNicknameController,
+                                onChanged: (String? value) {
+                                  _addressNicknameController.text = value ?? '';
+                                },
+                                validator: (String? value) =>
+                                    TValidator.validateEmptyText(
+                                        'Address Nickname', value),
+                              ),
+                              const SizedBox(height: TSize.s16),
+                              TextFormFieldComponent(
+                                textFieldWithTitle: true,
+                                title: 'Full Address',
+                                hintText: 'Enter your full address',
+                                controller: _fullAddressController,
+                              ),
+                              const SizedBox(height: TSize.s16),
+                              Row(
+                                children: [
+                                  Checkbox.adaptive(
+                                    value: isDefaultAddress,
+                                    onChanged: (bool? value) {
+                                      _onIsDefaultAddressChanged(value!);
+                                    },
+                                  ),
+                                  const TextWidget(
+                                    'Make this as a default address',
+                                  )
+                                ],
+                              ),
+                              const SizedBox(height: TSize.s16),
+                              ElevatedButton(
+                                onPressed: () {
+                                  context.pop();
+                                },
+                                child: const Text('Add'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+                child: Container(
+                  height: 64,
+                  width: double.infinity,
+                  padding: const EdgeInsets.symmetric(horizontal: TPadding.p06)
+                      .copyWith(top: TPadding.p16),
+                  decoration: BoxDecoration(
+                    color: theme.colorScheme.outline.withValues(alpha: 0.20),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  alignment: Alignment.topCenter,
+                  child: TextWidget(
+                    'Add Address',
+                    style: theme.textTheme.bodyLarge,
+                  ),
+                ),
+              );
+            },
+            onMapCreated: (controller) {
+              controller.setMapStyle(mapStyle);
+            },
             onPlacePicked: (LocationResult result) {
               context.pop<LocationResult>(result);
+
               log("Place picked: ${result.formattedAddress}");
             },
+            myLocationFABConfig: const MyLocationFABConfig(bottom: 24, right: 24),
             initialLocation: LatLng(position?.latitude ?? 0, position?.longitude ?? 0),
             usePinPointingSearch: true,
             searchInputConfig: const SearchInputConfig(
-              padding: EdgeInsets.symmetric(
-                horizontal: 16.0,
-                vertical: 8.0,
-              ),
+              padding: EdgeInsets.all(24),
               textDirection: TextDirection.ltr,
             ),
-
             enableNearbyPlaces: false,
+            autocompletePlacesSearchRadius: TSize.s08,
             searchInputDecorationConfig: SearchInputDecorationConfig(
               hintText: "Search for a building, street or ...",
               fillColor: theme.colorScheme.surface,
@@ -109,6 +232,60 @@ class _AddNewAddressScreenState extends State<AddNewAddressScreen> {
           );
         },
       ),
+      // bottomSheet: AnimatedContainer(
+      //   height: bottomSheet ? 400 : 0,
+      //   duration: const Duration(milliseconds: 300),
+      //   decoration: BoxDecoration(
+      //     color: theme.colorScheme.surface,
+      //     borderRadius: const BorderRadius.only(
+      //       topLeft: Radius.circular(TRadius.r16),
+      //       topRight: Radius.circular(TRadius.r16),
+      //     ),
+      //   ),
+      //   child: Padding(
+      //     padding: const EdgeInsets.all(TPadding.p20).copyWith(top: 0),
+      //     child: SingleChildScrollView(
+      //       child: Column(
+      //         children: [
+      //           GestureDetector(
+      //             onTap: () => _onShowBottomSheet(false),
+      //             onPanDown: (details) => _onShowBottomSheet(false),
+      //             child: Container(
+      //               height: 6,
+      //               width: 64,
+      //               margin: const EdgeInsets.symmetric(vertical: TPadding.p16),
+      //               decoration: BoxDecoration(
+      //                 color: theme.colorScheme.outline.withValues(alpha: 0.20),
+      //                 borderRadius: BorderRadius.circular(10),
+      //               ),
+      //             ),
+      //           ),
+      //           Row(
+      //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //             children: [
+      //               TextWidget(
+      //                 'Address',
+      //                 style: theme.textTheme.titleLarge,
+      //               ),
+      //               IconButton(
+      //                 onPressed: () => _onShowBottomSheet(false),
+      //                 icon: const Icon(
+      //                   Icons.close,
+      //                 ),
+      //               ),
+      //             ],
+      //           ),
+      //           ElevatedButton(
+      //             onPressed: () {
+      //               context.pop();
+      //             },
+      //             child: const Text('Add'),
+      //           ),
+      //         ],
+      //       ),
+      //     ),
+      //   ),
+      // ),
     );
   }
 }
